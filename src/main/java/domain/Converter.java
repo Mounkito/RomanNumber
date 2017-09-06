64d9b18880e8e964c8a291017010c08cb16c44e2
Yohan <yohanlamerand@gmail.com>
2017-09-06 18:17:33 +0200

    [## Refactor : case 1,2,3,5,6,7 and 8

--- src/main/java/domain/Converter.java
+++ src/main/java/domain/Converter.java
@@ -3,7 +3,12 @@ package domain;
 public class Converter {
     public String toRomanNumber(int number) {
         StringBuilder stringBuilder = new StringBuilder();
-        if(number == 1 || number == 2 || number == 3){
+
+        if(number >= 5 && number < 9){
+            stringBuilder.append("V");
+            number = number - 5;
+        }
+        if(number == 0 ||number == 1 || number == 2 || number == 3){
             for (int i = 0; i < number; i++) {
                 stringBuilder.append("I");
             }
@@ -13,16 +18,7 @@ public class Converter {
         if(number == 4)
             return "IV";
 
-        if(number == 5 || number == 6 || number == 7){
-            stringBuilder.append("V");
-            for (int i = 0; i < number - 5; i++) {
-                stringBuilder.append("I");
-            }
-            return stringBuilder.toString();
-        }
 
-        if(number == 8)
-            return "VIII";
         if(number == 9)
             return "IX";
         if(number == 10)
