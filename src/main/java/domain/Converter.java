589d67db930d882fd019c2711d86089e74d4e531
Yohan <yohanlamerand@gmail.com>
2017-09-06 17:45:45 +0200

    [##] Refactor  :  case 5 , 6 , 7 and 8

--- src/main/java/domain/Converter.java
+++ src/main/java/domain/Converter.java
@@ -9,16 +9,21 @@ public class Converter {
             }
             return stringBuilder.toString();
         }
+
         if(number == 4)
             return "IV";
-        if(number == 5)
-            return "V";
-        if(number == 6)
-            return "VI";
-        if(number == 7)
-            return "VII";
+
+        if(number == 5 || number == 6 || number == 7){
+            stringBuilder.append("V");
+            for (int i = 0; i < number - 5; i++) {
+                stringBuilder.append("I");
+            }
+            return stringBuilder.toString();
+        }
+
         if(number == 8)
             return "VIII";
+
         else return "";
     }
 }
