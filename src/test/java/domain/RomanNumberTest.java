369b87d31c458d5d5b7db0df63b9fae29bb425a6
Yohan <yohanlamerand@gmail.com>
2017-09-07 14:58:56 +0200

    [## Refactor :  adding test for find the closest roman number and rename test

--- src/test/java/domain/RomanNumberTest.java
+++ src/test/java/domain/RomanNumberTest.java
@@ -6,22 +6,29 @@ import org.junit.Test;
 public class RomanNumberTest {
 
     @Test
-    public void should_return_the_closest_roman_number_I() throws Exception {
+    public void should_return_the_closest_roman_number_I_with_two() throws Exception {
         Assertions.assertThat(RomanNumber.romanNumberclosest(2)).isEqualTo(RomanNumber.ONE);
     }
 
     @Test
-    public void should_return_the_closest_roman_number_V() throws Exception {
+    public void should_return_the_closest_roman_number_I_with_three() throws Exception {
         Assertions.assertThat(RomanNumber.romanNumberclosest(3)).isEqualTo(RomanNumber.ONE);
     }
 
+
     @Test
-    public void should_return_the_closest_roman_number_X() throws Exception {
+    public void should_return_the_closest_roman_number_V_with_four() throws Exception {
+        Assertions.assertThat(RomanNumber.romanNumberclosest(4)).isEqualTo(RomanNumber.FIVE);
+    }
+
+
+    @Test
+    public void should_return_the_closest_roman_number_X_with_twelve() throws Exception {
         Assertions.assertThat(RomanNumber.romanNumberclosest(12)).isEqualTo(RomanNumber.TEN);
     }
 
     @Test
-    public void should_return_the_closest_roman_number_L() throws Exception {
+    public void should_return_the_closest_roman_number_L_with_fourty_five() throws Exception {
         Assertions.assertThat(RomanNumber.romanNumberclosest(45)).isEqualTo(RomanNumber.FIFTY);
     }
 
