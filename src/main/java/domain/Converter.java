7bfe40cbea29c54b029ebc1ce7f2c617356fa781
Yohan <yohanlamerand@gmail.com>
2017-09-12 16:56:45 +0200

    [##] Refactor  :  delete useless code

--- src/main/java/domain/Converter.java
+++ src/main/java/domain/Converter.java
@@ -2,32 +2,27 @@ package domain;
 
 public class Converter {
 
-
     public String toRomanNumber(int valueToConvert) {
         StringBuilder stringBuilder = new StringBuilder();
         int restToProcess = valueToConvert;
         for (RomanNumber romanNumber : RomanNumber.values()) {
-            int valueToConvert1 = romanNumber.getArabicNumber();
-            int restToProcess1 = restToProcess;
-            while (restToProcess1 >= valueToConvert1) {
+            valueToConvert = romanNumber.getArabicNumber();
+            while (restToProcess >= valueToConvert) {
                 stringBuilder.append(romanNumber.getRomanNumber());
-                restToProcess1 -= valueToConvert1;
+                restToProcess -= valueToConvert;
             }
-            restToProcess = restToProcess1;
         }
         return stringBuilder.toString();
     }
 
     public int toArabicNumber(String numberToConvert) {
-        int countValue = 0;
+        int arabicResult = 0;
         for (RomanNumber romanNumber : RomanNumber.values()) {
-            int arabicResult = 0;
             while (numberToConvert.startsWith(romanNumber.getRomanNumber())){
                 arabicResult += romanNumber.getArabicNumber();
                 numberToConvert = numberToConvert.substring(romanNumber.getRomanNumber().length());
             }
-            countValue += arabicResult;
         }
-        return countValue;
+        return arabicResult;
     }
 }
