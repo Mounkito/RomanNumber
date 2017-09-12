c35a35696eb99063b2e2a341fdf9a28be7b1f346
Yohan <yohanlamerand@gmail.com>
2017-09-12 15:20:07 +0200

    [##] Refactor  :  RomanToArabic delete dead code

--- src/main/java/domain/Converter.java
+++ src/main/java/domain/Converter.java
@@ -4,16 +4,10 @@ public class Converter {
     public String toRomanNumber(int valueToConvert) {
         StringBuilder stringBuilder = new StringBuilder();
         int restToProcess = valueToConvert;
-
         for (RomanNumber romanNumber : RomanNumber.values()) {
             restToProcess = createRomanString(stringBuilder, romanNumber.getRomanNumber(), romanNumber.getArabicNumber(), restToProcess);
         }
-
-        for (int i = 0; i < restToProcess; i++) {
-            stringBuilder.append("I");
-        }
         return stringBuilder.toString();
-
     }
 
     private int createRomanString(StringBuilder stringBuilder, String romanNumber, int valueToConvert, int restToProcess) {
