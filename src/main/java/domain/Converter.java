69815bda792255033f547fd41603cd0697c7fe61
Yohan <yohanlamerand@gmail.com>
2017-09-12 16:53:59 +0200

    [##] Refactor  :  Using the same enum for both conversion

--- src/main/java/domain/Converter.java
+++ src/main/java/domain/Converter.java
@@ -1,44 +1,33 @@
 package domain;
 
 public class Converter {
+
+
     public String toRomanNumber(int valueToConvert) {
         StringBuilder stringBuilder = new StringBuilder();
         int restToProcess = valueToConvert;
         for (RomanNumber romanNumber : RomanNumber.values()) {
-            restToProcess = createRomanString(stringBuilder, romanNumber.getRomanNumber(), romanNumber.getArabicNumber(), restToProcess);
+            int valueToConvert1 = romanNumber.getArabicNumber();
+            int restToProcess1 = restToProcess;
+            while (restToProcess1 >= valueToConvert1) {
+                stringBuilder.append(romanNumber.getRomanNumber());
+                restToProcess1 -= valueToConvert1;
+            }
+            restToProcess = restToProcess1;
         }
         return stringBuilder.toString();
-    }
-
-    private int createRomanString(StringBuilder stringBuilder, String romanNumber, int valueToConvert, int restToProcess) {
-        while (restToProcess >= valueToConvert) {
-            stringBuilder.append(romanNumber);
-            restToProcess -= valueToConvert;
-        }
-        return restToProcess;
     }
 
     public int toArabicNumber(String numberToConvert) {
         int countValue = 0;
         for (RomanNumber romanNumber : RomanNumber.values()) {
-            countValue += CountCharValue(numberToConvert,romanNumber.getRomanNumber(),romanNumber.getNegativeSumIfExist());
+            int arabicResult = 0;
+            while (numberToConvert.startsWith(romanNumber.getRomanNumber())){
+                arabicResult += romanNumber.getArabicNumber();
+                numberToConvert = numberToConvert.substring(romanNumber.getRomanNumber().length());
+            }
+            countValue += arabicResult;
         }
         return countValue;
-    }
-
-    private int CountCharValue(String numberToConvert, String romanNumber, int arabicNumber) {
-        int countChar = 0;
-        for (int i = 0; i < numberToConvert.length(); i++) {
-            //Double element case ex : IV
-            if(i+2 <= numberToConvert.length() && numberToConvert.substring(i,i+2).equals(romanNumber)){
-                countChar += arabicNumber;
-                i++;
-            }
-            //One element case ex : V
-            if(numberToConvert.substring(i,i+1).equals(romanNumber)){
-                countChar += arabicNumber;
-            }
-        }
-        return countChar;
     }
 }
--- src/main/java/domain/RomanNumber.java
+++ src/main/java/domain/RomanNumber.java
@@ -1,32 +1,27 @@
 package domain;
 
-import java.util.OptionalInt;
-
 public enum RomanNumber {
-    THOUSAND("M", 1000,OptionalInt.empty()),
-    NINE_HUNDRED("CM", 900,OptionalInt.of(-200)),
-    FIVE_HUNDRED("D", 500, OptionalInt.empty()),
-    FOUR_HUNDRED("CD", 400, OptionalInt.of(-200)),
-    HUNDRED("C", 100, OptionalInt.empty()),
-    NINETY("XC", 90, OptionalInt.of(-20)),
-    FIFTY("L", 50, OptionalInt.empty()),
-    FORTY("XL", 40, OptionalInt.of(-20)),
-    TEN("X", 10, OptionalInt.empty()),
-    NINE("IX", 9, OptionalInt.of(-2)),
-    FIVE("V", 5, OptionalInt.empty()),
-    FOUR("IV", 4, OptionalInt.of(-2)),
-    ONE("I", 1, OptionalInt.empty());
+    THOUSAND("M", 1000),
+    NINE_HUNDRED("CM", 900),
+    FIVE_HUNDRED("D", 500),
+    FOUR_HUNDRED("CD", 400),
+    HUNDRED("C", 100),
+    NINETY("XC", 90),
+    FIFTY("L", 50),
+    FORTY("XL", 40),
+    TEN("X", 10),
+    NINE("IX", 9),
+    FIVE("V", 5),
+    FOUR("IV", 4),
+    ONE("I", 1);
 
     private final int arabicNumber;
-    private final OptionalInt negativeSum;
     private String romanNumber;
 
-    RomanNumber(String romanNumber, int arabicNumber, OptionalInt negativeSum) {
+    RomanNumber(String romanNumber, int arabicNumber) {
         this.romanNumber = romanNumber;
         this.arabicNumber = arabicNumber;
-        this.negativeSum = negativeSum;
     }
-
 
     public String getRomanNumber() {
         return romanNumber;
@@ -36,7 +31,4 @@ public enum RomanNumber {
         return arabicNumber;
     }
 
-    public int getNegativeSumIfExist() {
-        return negativeSum.orElse(arabicNumber);
-    }
 }
